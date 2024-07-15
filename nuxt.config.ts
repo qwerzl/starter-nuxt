a3088523b4c114ee8dc46864817f1cd2dd5e268b
qwerzl <me@qwerzl.me>
2024-07-15 16:47:06 +0800

    chore: add linter

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,5 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ["@nuxt/fonts", "shadcn-nuxt", "@nuxt/image", "@nuxtjs/tailwindcss"]
+  modules: ['@nuxt/fonts', 'shadcn-nuxt', '@nuxt/image', '@nuxtjs/tailwindcss'],
 })
