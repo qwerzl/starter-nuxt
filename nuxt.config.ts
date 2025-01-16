db202692539c2f4621f5357f28673357ff97de62
qwerzl <me@qwerzl.me>
2025-01-16 10:29:39 +0800

    chore: use `unocss-preset-shadcn`

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,14 @@ import { defineNuxtConfig } from "nuxt/config";
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@nuxt/fonts', 'shadcn-nuxt', '@nuxt/image', '@unocss/nuxt'],
+  modules: [
+    '@nuxt/fonts',
+    'shadcn-nuxt',
+    '@nuxt/image',
+    '@unocss/nuxt',
+    '@nuxtjs/color-mode',
+  ],
+  colorMode: {
+    dataValue: 'theme',
+  }
 })
--- uno.config.ts
+++ uno.config.ts
@@ -1,5 +1,26 @@
-import { defineConfig } from 'unocss'
+import { defineConfig, presetUno } from 'unocss'
+import presetAnimations from 'unocss-preset-animations'
+import { presetShadcn } from 'unocss-preset-shadcn'
 
 export default defineConfig({
-  // ...UnoCSS options
+  presets: [
+    presetUno(),
+    presetAnimations(),
+    presetShadcn({
+      darkSelector: '[data-theme="dark"]',
+    }),
+  ],
+  // By default, `.ts` and `.js` files are NOT extracted.
+  // If you want to extract them, use the following configuration.
+  // It's necessary to add the following configuration if you use shadcn-vue or shadcn-svelte.
+  content: {
+    pipeline: {
+      include: [
+        // the default
+        /\.(vue|svelte|[jt]sx|mdx?|astro|elm|php|phtml|html)($|\?)/,
+        // include js/ts files
+        '(components|src)/**/*.{js,ts}',
+      ],
+    },
+  },
 })
